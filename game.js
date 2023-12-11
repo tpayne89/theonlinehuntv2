c50558c27c686b17b9caea89cccbfb4b76b78360
tpayne89 <86229366+tpayne89@users.noreply.github.com>
2023-12-11 20:56:34 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
-// Firebase configuration (replace with your own config)
+// game.js
+import { useLiveData } from "./firebase";
+
+// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBGlJxzkEAlLJiu0GGNkIlNYmSJ30UtZ5o",
   authDomain: "theonlinehunt-3268b.firebaseapp.com",
@@ -6,54 +9,54 @@ const firebaseConfig = {
   projectId: "theonlinehunt-3268b",
   storageBucket: "theonlinehunt-3268b.appspot.com",
   messagingSenderId: "186291357760",
-  appId: "1:186291357760:web:88bd4aa13dc67d446f77a5"
+  appId: "1:186291357760:web:88bd4aa13dc67d446f77a5",
+  measurementId: "G-F5WE56KLB9",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const database = firebase.database();
+const questionContainer = document.getElementById("question-container");
+const answerInput = document.getElementById("answer-input");
+const submitAnswerButton = document.getElementById("submit-answer");
 
-let currentQuestion = 0;
+let currentQuestionIndex = 0;
 
-function showQuestion() {
-  const questionElement = document.getElementById("question");
-  console.log("questions:", questions); // Log questions array
-  questionElement.innerHTML = questions[currentQuestion]?.question || "Game Over!";
-}
+async function loadQuestion(index) {
+  const path = `questions/${index}`;
+  const { data, ready } = await useLiveData(path);
 
-function checkAnswer() {
-  const answerElement = document.getElementById("answer");
-  const userAnswer = answerElement.value;
-  const correctAnswer = questions[currentQuestion]?.answer;
-
-  console.log("User Answer:", userAnswer);
-  console.log("Correct Answer:", correctAnswer);
-
-  if (userAnswer && correctAnswer && userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-    alert("Correct answer!");
-    currentQuestion++;
-
-    if (currentQuestion < questions.length) {
-      showQuestion();
-      answerElement.value = "";
-    } else {
-      alert("Quiz completed!");
-      answerElement.disabled = true;
-    }
-  } else {
-    alert("Wrong answer. Try again.");
+  if (ready) {
+    questionContainer.innerHTML = data.question || "Game Over!";
   }
 }
 
-// Retrieve questions from the Firebase Realtime Database
-database.ref("questions").on("value", function(snapshot) {
-  const data = snapshot.val();
-  console.log("Firebase Data:", data); // Log Firebase data
+function checkAnswer(index, answer) {
+  const path = `questions/${index}/answer`;
 
-  if (data) {
-    questions = data;
-    // Show the first question when the page and data are loaded
-    showQuestion();
+  // Ensure the correct answer is retrieved before proceeding
+  useLiveData(path).then(({ data, ready }) => {
+    if (ready) {
+      const correctAnswer = data;
+      if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
+        currentQuestionIndex++;
+        loadQuestion(currentQuestionIndex);
+        answerInput.value = ""; // Clear the input field
+      } else {
+        alert("Incorrect answer. Try again!");
+      }
+    }
+  });
+}
+
+submitAnswerButton.addEventListener("click", () => {
+  const userAnswer = answerInput.value.trim();
+  if (userAnswer !== "") {
+    checkAnswer(currentQuestionIndex, userAnswer);
+  } else {
+    alert("Please enter an answer.");
   }
 });
+
+// Initial load
+loadQuestion(currentQuestionIndex);
